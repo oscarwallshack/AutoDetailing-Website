cfe3a184fb1f7fb71731afa09b490545de538ae3
Oscar <bo.walczak@wp.pl>
2022-11-06 13:12:14 +0100

    Skończony projekt wersja 1

---
+++ header.php
@@ -0,0 +1,27 @@
+<body>
+
+    <!-- Navbar Start -->
+
+    <nav id="mainNav" class="navbar navbar-dark navbar-expand-lg fixed-top">
+        <div class="container">
+            <a href="/wizytówka" class="navbar-brand">Walczak AutoDetailing</a>
+            <button type="button" class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div id="navbarResponsive" class="collapse navbar-collapse">
+                <div class="navbar-nav ms-auto">
+                    <a href="#" class="nav-item nav-link mx-4">Cennik</a>
+
+                    <div class="nav-item dropdown mx-4">
+                        <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Usługi</a>
+                        <div class="dropdown-menu">
+                            <a href="#" class="nav-link dropdown-item">Czyszczenie wnętrz</a>
+                            <a href="#" class="nav-link dropdown-item">Mycie zewnętrzne</a>
+                        </div>
+                    </div>
+                    <a href="#" class="nav-item nav-link mx-4">Kontakt</a>
+                </div>
+            </div>
+        </div>
+    </nav>
+    <!-- Navbar End -->
