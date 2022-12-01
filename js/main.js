f20153458ac8c37c9c86ab861662e15c1785c23a
Oscar <bo.walczak@wp.pl>
2022-12-01 23:32:26 +0100

    style zoom image i zamykanie zdjecia js

---
+++ js/main.js
@@ -0,0 +1,4 @@
+const closeBtn = document.querySelector('.close');
+const zoomImage = document.querySelector('.zoom-images');
+
+closeBtn.addEventListener('click', function()  { zoomImage.style.display = 'none'; })
