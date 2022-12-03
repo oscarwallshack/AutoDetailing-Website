047abe5072ed4d2a22905ff84ea3749c718eba76
Oscar <bo.walczak@wp.pl>
2022-12-03 12:43:21 +0100

    fixed zoom image

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 const closeBtn = document.querySelector('.close');
+const images = document.querySelector('.images');
 const zoomImage = document.querySelector('.zoom-images');
 
 closeBtn.addEventListener('click', function()  { zoomImage.style.display = 'none'; })
